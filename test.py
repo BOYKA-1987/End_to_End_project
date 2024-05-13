cb31fe7bdad02c08c93033a42d1e9c8876574446
Vipul More <vipul1987more@gmail.com>
2024-05-14 02:57:06 +0530

    template file added

---
+++ test.py
@@ -0,0 +1,30 @@
+import os
+path="notebooks/research.ipynb"
+
+dir,file=os.path.split(path)
+
+os.makedirs(dir,exist_ok=True)
+
+with open (path,"w") as f:
+    pass
+
+
+
+"""echo [$(date)]: "START"
+
+
+echo [$(date)]: "creating env with python 3.8 version" 
+
+
+conda create --prefix ./env python=3.8 -y
+
+
+echo [$(date)]: "activating the environment" 
+
+source activate ./env
+
+echo [$(date)]: "installing the dev requirements" 
+
+pip install -r requirements.txt
+
+echo [$(date)]: "END"  """
