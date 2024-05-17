f5e84e984894d82434ef3a4df1906781e0ef74c5
Vipul More <vipul1987more@gmail.com>
2024-05-17 17:23:59 +0530

    eigth commit

--- src/DimondPricePrediction/logger.py
+++ src/DimondPricePrediction/logger.py
@@ -9,11 +9,12 @@ log_path=os.path.join(os.getcwd(),"logs")
 
 os.makedir(log_path,exit_ok=True)
 
-LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
+LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
 
 logging.basicConfig(level=logging.INFO,
-                    filename=LOG_FILE_PATH,
+                    filename=LOGFILE_PATH,
                     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+
 )
 
 
