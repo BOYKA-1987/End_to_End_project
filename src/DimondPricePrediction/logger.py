09bf2957e74f1394ce46387ebe8432b2d51c1ad1
Vipul More <vipul1987more@gmail.com>
2024-05-18 14:42:22 +0530

    Exception_handing

--- src/DimondPricePrediction/logger.py
+++ src/DimondPricePrediction/logger.py
@@ -7,7 +7,7 @@ LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_path=os.path.join(os.getcwd(),"logs")
 
-os.makedir(log_path,exit_ok=True)
+os.makedirs(log_path,exist_ok=True)
 
 LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
 
@@ -17,5 +17,6 @@ logging.basicConfig(level=logging.INFO,
 
 )
 
-
+if __name__ == '__main__':
+    logging.info("here again i am testing")
 
