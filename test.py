cd68d964096603636022ce798cb58b2bc2fcee28
Vipul More <vipul1987more@gmail.com>
2024-05-23 22:21:05 +0530

    16

--- test.py
+++ test.py
@@ -29,8 +29,13 @@ pip install -r requirements.txt
 
 echo [$(date)]: "END"  """
 
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData
 
+CustDataobj=CustomData(1.52,62.2,58.0,7.27,7.33,4.55,"Premium","F","VS2")
 
+data=CustDataobj.get_data_as_dataframe()
+
+print(data)
 
 
 
